1599b80c0e6b01e008aa683a985a2551e064c93e
Clemens Horn <clemens.horn@mni.thm.de>
2024-09-24 13:27:25 +0200

    feat: add cachix cache to flake

--- flake.nix
+++ flake.nix
@@ -1,4 +1,15 @@
 {
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+      "https://clemenscodes.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "clemenscodes.cachix.org-1:yEwW1YgttL2xdsyfFDz/vv8zZRhRGMeDQsKKmtV1N18="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
@@ -8,36 +19,39 @@
     };
   };
 
-  outputs = inputs: with inputs; flake-utils.lib.eachDefaultSystem (
-    system: let
-      inherit (nixpkgs) lib;
-      overlays = [(import rust-overlay)];
-      pkgs = import nixpkgs { inherit system lib overlays; };
-      bin = pkgs.pkgsBuildHost.rust-bin;
-      rustToolchain = bin.fromRustupToolchainFile ./rust-toolchain.toml;
-      extendedRustToolchain = rustToolchain.override {
-        extensions = [
-          "rust-src"
-          "clippy"
-          "llvm-tools"
-        ];
-      };
-    in with pkgs; {
-      packages = {
-        default = import ./default.nix { inherit pkgs; };
-      };
-      devShells = {
-        default = mkShell {
-          buildInputs = [pkg-config];
-          nativeBuildInputs = [
-            extendedRustToolchain
-            rust-analyzer
-            openssl
-          ];
-          RUST_SRC_PATH = "${rust.packages.stable.rustPlatform.rustLibSrc}";
-          RUST_BACKTRACE = 1;
-        };
-      };
-    }
-  );
+  outputs = inputs:
+    with inputs;
+      flake-utils.lib.eachDefaultSystem (
+        system: let
+          inherit (nixpkgs) lib;
+          overlays = [(import rust-overlay)];
+          pkgs = import nixpkgs {inherit system lib overlays;};
+          bin = pkgs.pkgsBuildHost.rust-bin;
+          rustToolchain = bin.fromRustupToolchainFile ./rust-toolchain.toml;
+          extendedRustToolchain = rustToolchain.override {
+            extensions = [
+              "rust-src"
+              "clippy"
+              "llvm-tools"
+            ];
+          };
+        in
+          with pkgs; {
+            packages = {
+              default = import ./default.nix {inherit pkgs;};
+            };
+            devShells = {
+              default = mkShell {
+                buildInputs = [pkg-config];
+                nativeBuildInputs = [
+                  extendedRustToolchain
+                  rust-analyzer
+                  openssl
+                ];
+                RUST_SRC_PATH = "${rust.packages.stable.rustPlatform.rustLibSrc}";
+                RUST_BACKTRACE = 1;
+              };
+            };
+          }
+      );
 }
