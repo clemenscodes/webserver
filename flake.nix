bb1a25413a679d5e90aa86ed8ee319f2360b5247
Clemens Horn <clemens.horn@mni.thm.de>
2024-09-24 19:37:01 +0200

    fix(ci): lockdown nix src input using nix-filter

--- flake.nix
+++ flake.nix
@@ -1,18 +1,8 @@
 {
-  nixConfig = {
-    extra-substituters = [
-      "https://nix-community.cachix.org"
-      "https://clemenscodes.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "clemenscodes.cachix.org-1:yEwW1YgttL2xdsyfFDz/vv8zZRhRGMeDQsKKmtV1N18="
-    ];
-  };
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    nix-filter.url = "github:numtide/nix-filter";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -25,6 +15,7 @@
         system: let
           inherit (nixpkgs) lib;
           overlays = [(import rust-overlay)];
+          filter = nix-filter.lib;
           pkgs = import nixpkgs {inherit system lib overlays;};
           bin = pkgs.pkgsBuildHost.rust-bin;
           rustToolchain = bin.fromRustupToolchainFile ./rust-toolchain.toml;
@@ -38,7 +29,7 @@
         in
           with pkgs; {
             packages = {
-              default = import ./default.nix {inherit pkgs;};
+              default = import ./default.nix {inherit pkgs filter;};
             };
             devShells = {
               default = mkShell {
@@ -54,4 +45,15 @@
             };
           }
       );
+
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+      "https://clemenscodes.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "clemenscodes.cachix.org-1:yEwW1YgttL2xdsyfFDz/vv8zZRhRGMeDQsKKmtV1N18="
+    ];
+  };
 }
